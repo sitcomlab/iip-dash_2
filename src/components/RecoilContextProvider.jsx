0b9d27a59386dc1455f8036ddf229c80d221a374
niebl <caro.niebl@uni-muenster.de>
2024-11-06 12:26:27 +0100

    style: indent

--- src/components/RecoilContextProvider.jsx
+++ src/components/RecoilContextProvider.jsx
@@ -11,27 +11,26 @@ export const todoListState = atom({
 //the state that determines which city is currently viewed.
 //  New configs are stored within the page.js of each new city
 export const cityViewConfigState = atom({
-    key: 'CityViewConfig',
-    default: {
-        name: 'Münster',
-        mapSettings: 
-            {
-                center: [51.962, 7.627],
-                zoom: 12
-            },
-        infrastructureSource: process.env.NEXT_PUBLIC_BICYCLE_INFRASTRUCTURE_URL_MS
-    } //default at Münster
-})
+  key: "CityViewConfig",
+  default: {
+    name: "Münster",
+    mapSettings: {
+      center: [51.962, 7.627],
+      zoom: 12,
+    },
+    infrastructureSource: process.env.NEXT_PUBLIC_BICYCLE_INFRASTRUCTURE_URL_MS,
+  }, //default at Münster
+});
 
 //The following 3 states states dictate selection and display in the AdministrativeArea mapview
 export const selectedAAState = atom({
-    key: 'selectedAA',
-    default: ''
-})
+  key: "selectedAA",
+  default: "",
+});
 export const selectedAAFeatureState = atom({
-    key: 'selectedAAFeature',
-    default: []
-})
+  key: "selectedAAFeature",
+  default: [],
+});
 /*
 possible PointDataTypes include:
     false = 'keine',
@@ -46,10 +45,9 @@ possible PointDataTypes include:
     fahrradAmpel = 'Fahrrad-Ampel'
 */
 export const displayedPointDataState = atom({
-    key: 'displayedPointData',
-    default: false
-})
-
+  key: "displayedPointData",
+  default: false,
+});
 
 export default function RecoilContextProvider({ children }) {
   return <RecoilRoot>{children}</RecoilRoot>;
